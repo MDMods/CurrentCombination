f574474658aaf3167cd7afeb595a950952fe004f
Asgragrt <131921758+Asgragrt@users.noreply.github.com>
2024-02-12 15:39:59 -0600

    Initial Commit

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using MelonLoader;
+using System.Reflection;
+using Main = CurrentCombination.Main;
+
+[assembly: MelonInfo(typeof(Main), "CurrentCombination", "1.0.0", "Asgragrt")]
+[assembly: MelonGame("PeroPeroGames", "MuseDash")]
+[assembly: MelonColor(255, 241, 196, 15)]
+
+[assembly: AssemblyTitle("CurrentCombination")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Muse Dash Modding Community")]
+[assembly: AssemblyProduct("CurrentCombination")]
+[assembly: AssemblyCopyright("Copyright © 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ README.md
@@ -0,0 +1,7 @@
+# CurrentCombination
+
+Shows the current selected character and elfin.
+
+![Song select](Media/SongSelect.jpg)
+
+![alt text](Media/SongMenu.jpg)
