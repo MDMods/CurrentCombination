9993c6c6e993bcfcf1b99b0ff04e43eb604b7139
Asgragrt <131921758+Asgragrt@users.noreply.github.com>
2024-04-25 20:12:00 -0600

    :lipstick: Changed position and fixed object name

--- Models/PrepElfinText.cs
+++ Models/PrepElfinText.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 
 namespace CurrentCombination.Models;
 
-internal class PrepElfinText() : BaseText(nameof(PrepGirlText), new Vector3(7.5f, -5f, 100f))
+internal class PrepElfinText() : BaseText(nameof(PrepElfinText), new Vector3(7.5f, -5.05f, 100f))
 {
     protected override string GetText() => ModManager.GetElfin();
 }
--- Models/StageText.cs
+++ Models/StageText.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 
 namespace CurrentCombination.Models;
 
-internal class StageText() : BaseText(nameof(StageText), new Vector3(6f, -5f, 100f))
+internal class StageText() : BaseText(nameof(StageText), new Vector3(6f, -5.05f, 100f))
 {
     protected override string GetText()
     {
